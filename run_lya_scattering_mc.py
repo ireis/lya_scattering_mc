b8e3e91dd549815c3c11eab8fa17db62552e1635
itamar <itamarreis@mail.tau.ac.il>
2019-08-26 18:05:37 +0300

    script for running the thing

---
+++ run_lya_scattering_mc.py
@@ -0,0 +1,22 @@
+import lya_scattering_mc as lya
+import numpy
+
+
+z_source_grid = numpy.arange(5,50)
+numpy.save('5000_nu_bins/z_source_grid', z_source_grid)
+
+
+# The code returns -nof_R- radii in each of -nof_nu_bins- nu bins (the bins are between lyman alpha and lyman beta)
+# for a given redshift.
+for z in z_source_grid:
+    print('At z = {}'.format(z))
+    lya_scatter = lya.LyaSctr_MC(z_s=z, nof_nu_bins=5000, nof_R=2000)
+    lya_scatter.run()
+    z_abs = lya_scatter.absorption_zs.flatten()
+    log_r = numpy.log10(lya_scatter.R_all.value.flatten())
+    
+    fname_log_r = '5000_nu_bins/log_r_z_{}'.format(z)
+    fname_z_abs = '5000_nu_bins/z_abs_z_{}'.format(z)
+    
+    numpy.save(fname_log_r, log_r)
+    numpy.save(fname_z_abs, z_abs)
